a706fb0dda7cbb6e194b11ecca0dd06afeea29f8
0xMailMan <chengye.pcy@gmail.com>
2026-03-05 16:29:49 +0800

    style: replace broswer favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,15 +19,7 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       {
-        url: '/icon-light-32x32.png',
-        media: '(prefers-color-scheme: light)',
-      },
-      {
-        url: '/icon-dark-32x32.png',
-        media: '(prefers-color-scheme: dark)',
-      },
-      {
-        url: '/icon.svg',
+        url: '/logo-OIC.svg',
         type: 'image/svg+xml',
       },
     ],
